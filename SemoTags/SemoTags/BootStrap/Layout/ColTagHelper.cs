081f38aa75d44d4dba2a47d0c1f5b4f800920f2e
SamerAbdAlla <smsssamer@live.com>
2022-08-01 10:39:40 +0300

    add col size

---
+++ SemoTags/SemoTags/BootStrap/Layout/ColTagHelper.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SemoTags.Common;
+
+namespace SemoTags.BootStrap.Layout;
+
+public class ColTagHelper : TagHelper
+{
+    public BootStrapSize Size { get; set; } = BootStrapSize.None;
+    public ColumnSize ColumnSize { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "div";
+        var className = "col-";
+        if (Size != BootStrapSize.None)
+            className += Size.ToString().ToLower();
+        if (ColumnSize != ColumnSize.None)
+            className += "-" + ColumnSize.ToString();
+        output.Attributes.Add("class", className);
+    }
+}
