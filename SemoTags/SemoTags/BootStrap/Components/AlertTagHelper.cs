3a0684a94b1d4b5765922303834bd317c440459c
samer abdulla <smsssamer@live.com>
2022-08-16 01:12:26 +0300

    Adding Alert class

---
+++ SemoTags/SemoTags/BootStrap/Components/AlertTagHelper.cs
@@ -0,0 +1,14 @@
+﻿ 
+
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SemoTags.BootStrap.Components
+{
+    public class AlertTagHelper:TagHelper
+    {
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            return base.ProcessAsync(context, output);
+        }
+    }
+}
