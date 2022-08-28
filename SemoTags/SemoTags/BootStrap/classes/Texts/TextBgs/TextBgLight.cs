4557b71ef0da47e753d9a0cbe7169f63c10edb59
samer abdulla <smsssamer@live.com>
2022-08-28 16:22:07 +0300

    finshing bg text

---
+++ SemoTags/SemoTags/BootStrap/classes/Texts/TextBgs/TextBgLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SemoTags.BootStrap.classes.Texts.TextBgs;
+
+[HtmlTargetElement(Attributes = "text-bg-light")]
+public class TextBgLight : BaseTextTagHelper
+{
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.Attributes.RemoveAll("text-bg-light");
+        if (output.Attributes.All(p => p.Name != "class"))
+            output.Attributes.Add("class", "text-bg-light");
+        else
+            output.AddClass("text-bg-light", HtmlEncoder.Default);
+    }
+}
---
+++ SemoTags/SemoTags/BootStrap/classes/Texts/TextBgs/TextBgSuccess.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SemoTags.BootStrap.classes.Texts.TextBgs;
+
+[HtmlTargetElement(Attributes = "text-bg-success ")]
+public class TextBgSuccess : BaseTextTagHelper
+{
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.Attributes.RemoveAll("text-bg-success");
+        if (output.Attributes.All(p => p.Name != "class"))
+            output.Attributes.Add("class", "text-bg-success");
+        else
+            output.AddClass("text-bg-success", HtmlEncoder.Default);
+    }
+}
